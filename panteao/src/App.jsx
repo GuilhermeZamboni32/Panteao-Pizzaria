6cf58d8452567f580c92a69dda9fd29f7d8a9e9e
thpixel-dev <Gamernooob1673@gmail.com>
2025-08-14 16:47:49 -0300

    pasta back

---
+++ panteao/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { GlobalContextProvider } from '../Context/GlobalContext'
+
+function App() {
+ 
+  return (
+    <>
+      <GlobalContextProvider>
+        <h1>Router</h1>
+      </GlobalContextProvider>
+    </>
+  )
+}
+
+export default App
--- panteao/src/main.jsx
+++ panteao/src/main.jsx
@@ -4,7 +4,6 @@ import './index.css'
 import App from './App.jsx'
 import { RouterProvider } from 'react-router-dom'
 import router from './routers/Router.jsx'
-import { GlobalContextProvider } from './Context/GlobalContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <GlobalContextProvider>
