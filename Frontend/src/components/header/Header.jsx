ab0129db8d52c6d73b8da5996fd46480bc060305
GuilhermeZamboni32 <guilhermezamboni867@gmail.com>
2025-09-09 21:36:35 -0300

    pasta components

---
+++ Frontend/src/components/header/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./Header .css";
+
+function Header() {
+  return (
+     <div className="container-navbar">
+      <div className="logo">
+        <Link className="espaco-logo" to="/"><img className="logovf" src="LogoVF.png" alt="" /></Link>
+        <span className="logo-texto">VidaFit</span>
+      </div>
+
+      <div className="nav-links">
+        <Link className="texto" to="/home">Seleção de Profissionais</Link>
+        <Link className="texto" to="/Avaliacao">Avaliação</Link>
+        <Link className="texto" to="/perfil">Perfil</Link>
+        {/** */}
+        <Link className="texto" to="/telaexeredit">Exercicios</Link>
+        <Link className="texto" to="/teladietaedit">Dietas</Link>
+        <Link className="texto" to="/av_notas">Avaliações</Link>
+      </div>
+    </div>
+  )
+}
+
+export default Header
